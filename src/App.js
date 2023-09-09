28993ffdad4314d2ac67e0f56a2f271018b012ec
Harshit Gupta <hgtgt07@gmail.com>
2023-09-09 13:11:45 +0000

    site title, desktop Card component adjust

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ const foodItems = [
     price: "$10.99"
   },
   {
-    foodName: "Sushi",
-    img: "https://source.unsplash.com/800x600/?sushi",
+    foodName: "Masala Dosa",
+    img: "https://source.unsplash.com/800x600/?dosa",
     price: "$12.49"
   },
   {
@@ -27,6 +27,31 @@ const foodItems = [
     foodName: "Salad",
     img: "https://source.unsplash.com/800x600/?salad",
     price: "$7.99"
+  },
+  {
+    foodName: "Taco",
+    img: "https://source.unsplash.com/800x600/?taco",
+    price: "$6.99"
+  },
+  {
+    foodName: "Cappuccino",
+    img: "https://source.unsplash.com/800x600/?cappuccino",
+    price: "$15.99"
+  },
+  {
+    foodName: "Sushi Roll",
+    img: "https://source.unsplash.com/800x600/?sushi-roll",
+    price: "$11.49"
+  },
+  {
+    foodName: "Cheese Grilled Sandwich",
+    img: "https://source.unsplash.com/800x600/?veg-cheese-grilled-sandwich",
+    price: "$8.49"
+  },
+  {
+    foodName: "Ice Cream",
+    img: "https://source.unsplash.com/800x600/?ice-cream",
+    price: "$4.99"
   }
 ];
 
@@ -34,6 +59,7 @@ export default function App() {
   return (
     <div className="App">
       <Nav />
+      <h1 className="company-name">Foodie Hoomans</h1>
       <div className="foodItems">
         {foodItems.map((x) => {
           return <Cards img={x.img} foodName={x.foodName} price={x.price} />;
